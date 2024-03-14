3cae88dcca2e7919d0cd14de0ecee963564d4e5b
Schlumen <lumen.haendler@web.de>
2024-03-14 12:10:28 +0100

    Video generation

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { Download, VideoIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Heading from "@/components/heading";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { formSchema } from "./constants";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import Link from "next/link";
+
+const VideoPage = () => {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+
+      const response = await axios.post("/api/video", values);
+
+      setVideo(response.data[0]);
+      form.reset();
+    } catch (error: any) {
+      //Tdoo: Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Turn your prompts into video."
+        icon={VideoIcon}
+        iconColor="text-orange-500"
+        bgColor="bg-orange-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        autoComplete="off"
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Clown fish swimming around a coral reef"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4 mb-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!video && !isLoading && (
+            <div>
+              <Empty
+                label="No video generated."
+                imagePath="/empty_video.png"
+              />
+            </div>
+          )}
+          {video && (
+            <video
+              className="w-full max-w-5xl mx-auto aspect-video mt-8 rounded-lg border"
+              controls
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage;
